5504b13a73af1c3d7546aa7d6d1ba560282acaac
Wauplin <lucainp@gmail.com>
2022-11-23 09:59:51 +0100

    Sync using 'upload_folder' + add private attribute

--- sync_with_spaces.py
+++ sync_with_spaces.py
@@ -1,46 +1,44 @@
-from pathlib import Path
-from huggingface_hub import upload_file, create_repo
+from huggingface_hub import create_repo, upload_folder, whoami
+
 
 def main(
-    repo_id,
-    directory,
-    token,
-    repo_type='space',
-    space_sdk='gradio',
+    repo_id: str,
+    directory: str,
+    token: str,
+    repo_type: str = "space",
+    space_sdk: str = "gradio",
+    private: bool = False,
 ):
     print("Syncing with Hugging Face Spaces...")
+
+    if "/" not in repo_id:
+        # Case namespace is implicit
+        username = whoami(token=token)["name"]
+        repo_id = f"{username}/{repo_id}"
     print(f"\t- Repo ID: {repo_id}")
+
     print(f"\t- Directory: {directory}")
     url = create_repo(
         repo_id,
         token=token,
         exist_ok=True,
         repo_type=repo_type,
-        space_sdk=space_sdk if repo_type == 'space' else None
+        space_sdk=space_sdk if repo_type == "space" else None,
+        private=private,
     )
     print(f"\t- Repo URL: {url}")
-    for filepath in Path(directory).glob("**/*"):
 
-        if not filepath.is_file():
-            continue
+    # Sync folder
+    commit_url = upload_folder(
+        folder_path=directory,
+        repo_id=repo_id,
+        repo_type=repo_type,
+        token=token,
+        commit_message="Synced repo using 'sync_with_huggingface' Github Action",
+        ignore_patterns=["*.git*", "*README.md*"],
+    )
+    print(f"\t- Repo synced: {commit_url}")
 
-        if ".git" in str(filepath):
-            print(f"\t\t- Skipping {filepath} because it is in .git directory")
-            continue
-
-        if filepath.name == "README.md":
-            print(f"\t\t- Skipping {filepath} because it is a GitHub README")
-            continue
-
-        path_in_repo = str(filepath.relative_to(Path(directory)))
-        print(f"\t\t- Uploading '{filepath}' to '{path_in_repo}'")
-        upload_file(
-            path_or_fileobj=str(filepath),
-            path_in_repo=path_in_repo,
-            token=token,
-            repo_id=repo_id,
-            repo_type=repo_type,
-        )
 
 if __name__ == "__main__":
     from fire import Fire
